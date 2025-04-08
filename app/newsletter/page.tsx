db49fe96249393aa1554029e7997385a0075c91d
Favourite <favourson71@gmail.com>
2025-04-08 14:02:39 +0100

    edit field/form questions

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewsLetter from '../components/NewsLetter'
+
+export default function newsLetter() {
+  return (
+    <main>
+        <NewsLetter />
+    </main>
+  )
+}
