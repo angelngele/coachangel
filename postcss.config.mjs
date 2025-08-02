21e556d6d082d1648cbe53f47526c40e82a60886
Favourite <favourson71@gmail.com>
2025-08-02 16:02:00 +0100

    Homepage revamp & tailwind upgrade config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+const config = {
+  plugins: [tailwindcss, autoprefixer],
 };
+
+export default config;
