af5999913184a9dad1a329239a3ee15e3e8bb3ff
Favour <favourson71@gmail.com>
2025-10-07 11:20:15 +0100

    feat:edit 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function NotFound() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-800"><span>404 - Wetin you dey find no dey here,</span><span className="block">you fit message the owner of this site</span></h1>
-            <Link href="/" className="mt-6 px-6 py-2 bg-black text-white rounded-lg hover:bg-green-700">Go Home
+            <h1 className="text-4xl font-bold text-gray-800"><span>404 - Wetin you dey find no dey here,</span><span className="block">you fit email the owner of this site sha</span></h1>
+            <Link href="/" className="mt-6 px-6 py-2 bg-black text-white rounded-lg hover:bg-green-700">Go Home or Go Hard 😉
             </Link>
         </div>
     );
